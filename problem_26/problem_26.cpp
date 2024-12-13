97661b45314e9dbb4574347645a24b5b42dd703c
Amine aichouba <aminaihouba@gmail.com>
2024-12-13 17:21:12 +0100

    Update problem_26.cpp

--- problem_26/problem_26.cpp
+++ problem_26/problem_26.cpp
@@ -1,15 +1,1 @@
 
-
-#include <iostream>
-#include <boost/multiprecision/cpp_int.hpp>
-
-using namespace boost::multiprecision ;
-
-int main ()
-{
-    for ( cpp_int d = 1 ; d <= 1000 ; d ++ )
-        {
-            cpp_int max = ( 1 / d ) ; 
-        }
-    return 0 ;
-}
